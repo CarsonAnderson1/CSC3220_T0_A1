d8c2d72f6d09a1c9283c1ba7e6684a37fa0929d9
CarsonAnderson1 <andersonc20@spu.edu>
2023-02-27 12:49:32 -0800

    update

--- newMonkey/gorilla/App.js
+++ newMonkey/gorilla/App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from 'react-bootstrap/Button';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
