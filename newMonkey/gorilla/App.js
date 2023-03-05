07d6a884da1c43b47355b48aaf4212ef0b8535f7
RaygunBoi <97078099+RaygunBoi@users.noreply.github.com>
2023-03-05 15:39:36 -0800

    Updated App.js

--- newMonkey/gorilla/App.js
+++ newMonkey/gorilla/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const categories = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+
+      <View style={styles.homeButton}>
+        <Button
+          title = "Home"
+        />
+      </View>
+      
+      <ScrollView style={styles.scrollView}>
+        <Text>"Testing"</Text>
+      </ScrollView>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  homeButton: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingTop: 35,
+    paddingLeft: 25,
+  },
+  scrollView: {
+    backgroundColor: '#8e8d96',
+    marginHorizontal: 20,
+    flexDirection: 'column',
+  }
 });
