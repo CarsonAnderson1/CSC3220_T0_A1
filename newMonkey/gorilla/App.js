0f0891a875f5c9bfcf04ee470141e4bcd77bb0a6
RaygunBoi <97078099+RaygunBoi@users.noreply.github.com>
2023-03-05 17:01:00 -0800

    General layout for categories page completed

--- newMonkey/gorilla/App.js
+++ newMonkey/gorilla/App.js
@@ -12,13 +12,54 @@ export default function App() {
 
       <View style={styles.homeButton}>
         <Button
-          title = "Home"
+          title="Home"
+          color='#474745'
         />
       </View>
-      
-      <ScrollView style={styles.scrollView}>
-        <Text>"Testing"</Text>
-      </ScrollView>
+
+      <View style={styles.title}>
+        <Text style={{fontSize: 50}}>SafeSpending</Text>
+      </View>
+
+      <View style={styles.rowContainer}>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.addButton}>
+            <Button
+              title="+"
+              color='#10eb18'
+            />
+          </View>
+
+          <View style={styles.removeButton}>
+            <Button
+              title="-"
+              color='#e30707'
+            />
+          </View>
+        </View>
+        
+        <View style={styles.scrollAdjusts}>
+          <ScrollView style={styles.scrollView}>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+            <Text style={{fontSize: 30}}>"Testing"</Text>
+          </ScrollView>
+        </View>
+      </View>
 
     </View>
   );
@@ -29,15 +70,43 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  rowContainer: {
+    flexDirection: 'row-reverse',
+  },
+  buttonsContainer: {
+    flexDirection: 'column',
+  },
+  title: {
+    alignItems: 'center',
+  },
   homeButton: {
     alignItems: 'flex-start',
-    justifyContent: 'center',
     paddingTop: 35,
-    paddingLeft: 25,
+    paddingLeft: 15,
   },
   scrollView: {
-    backgroundColor: '#8e8d96',
+    backgroundColor: '#d4d9d6',
     marginHorizontal: 20,
-    flexDirection: 'column',
+    borderColor: '#080808',
+    borderWidth: 2,
+  },
+  scrollAdjusts: {
+    height: 420,
+    width: 390,
+    paddingTop: 40,
+    paddingRight: 10,
+    paddingLeft: 40,
+  },
+  addButton: {
+    alignItems: 'flex-end',
+    paddingTop: 115,
+    paddingRight: 35,
+    paddingLeft: 5,
+  },
+  removeButton: {
+    alignItems: 'flex-end',
+    paddingTop: 55,
+    paddingRight: 35,
+    paddingLeft: 5,
   }
 });
