79a4303dd6d3cf55ac360871771db0443475c51f
CarsonAnderson1 <andersonc20@spu.edu>
2023-03-11 15:22:50 -0800

    most recent

--- newMonkey/gorilla/components/TransactionsCreate.js
+++ newMonkey/gorilla/components/TransactionsCreate.js
@@ -1,12 +1,112 @@
 import {StyleSheet, Text, View, Button, Modal, props, TextInput,Font} from 'react-native'
-import {useState} from "react"
+import {useState, useEffect} from "react"
 function CreateTransaction(props){
-  const[catVisible, setCatVisible] = useState('');
-  const[monVisible, setMonVisible] = useState('');
-  const[dateVisible, setDateVisible] = useState('');
-  const[noteVisible, setNoteVisible] = useState('');
-  const[transaction,setTransaction] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
+  const [cat, setCat] = useState([]); // array that holds name list
+  const [currCat, setCurrCat] = useState(undefined); // for text input box
+  const [money, setMoney] = useState([]); // array that holds name list
+  const [currMoney, setCurrMoney] = useState(undefined); // for text input box
+  const [date, setDate] = useState([]); // array that holds name list
+  const [currDate, setCurrDate] = useState(undefined); // for text input box
+  const [note, setNote] = useState([]); // array that holds name list
+  const [currNote, setCurrNote] = useState(undefined); // for text input box
 
+
+  const db = SQLite.openDatabase("categories.db"); 
+
+  useEffect(() => {
+    db.transaction(tx => {
+      let sqlcmd = "";
+      sqlcmd += "CREATE TABLE IF NOT EXISTS categories";
+      sqlcmd += "  (id INTEGER PRIMARY KEY AUTOINCREMENT,";
+      sqlcmd += "   cat TEXT),";
+      sqlcmd += "   money INTEGER,";
+      sqlcmd += "   date TEXT,";
+      sqlcmd += "   note TEXT)";
+      tx.executeSql(sqlcmd);
+    });
+
+    db.transaction(tx => {
+      let sqlcmd = "SELECT * FROM categories";
+      tx.executeSql(sqlcmd, [],
+        (_, resultSet) => {
+          setName(resultSet.rows._array);  // results returned
+        }
+      );
+    });
+
+    setDataLoading(false);
+     
+  }, []);
+  
+  if (dataLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading categories...</Text>
+      </View>
+    );
+  }
+
+  const addCategory = () => {
+    db.transaction(tx => {
+      let sqlcmd = "";
+      sqlcmd += "INSERT INTO categories (name) values (?)";
+      tx.executeSql(sqlcmd, [currName],
+          (_, resultSet) => {
+          let existingName = [...name];
+          existingName.push({ id: resultSet.insertId, name: currName});
+          setName(existingName);
+        })
+    });
+  }
+
+  const showCategories = () => {
+    return name.map((assObj) => {
+      return (
+        <View key={assObj.id} style={styles.row}> 
+          <Text>{assObj.name}</Text>
+
+
+        </View>
+      );
+    });
+  };
+
+
+    return(
+      
+        <Modal visible = {props.visibleA} animationType = "slide">
+          <View style = {styles.buttons}>
+            <View style={styles.backButton}>
+              <Button
+                color = "red"
+                title = "cancel"
+                onPress={props.onCancelA}
+              />
+            </View>
+              <View style = {styles.confirmButton}>
+                <Button  title = "Confirm Creation" color= "green" style = {styles.addButton} width = "40%" onPress = {addCategory}> </Button>
+              </View>
+          </View>
+
+          <View style = {styles.appContainer}>
+              <View style = {styles.box}>
+                <Text style = {styles.textStyle}>
+                  Input Category to Create
+                </Text>
+                <TextInput 
+                  value = {currName}
+                  style = {styles.textInput} 
+                  placeholder = "ex. groceries" 
+                  onChangeText = {setCurrName}
+                />
+              </View>
+              <View style = {styles.column}>{showCategories()}</View>
+ 
+            </View>
+        </Modal>
+        
+    )
   function handleCat(enteredCatText){
     setCatVisible(enteredCatText);
   }
